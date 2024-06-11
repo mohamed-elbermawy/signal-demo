00096e46458d5a0ae669bfeef02b04e36a11acfa
mohamed-elbermawy <mohamed77bermawy@gmail.com>
2024-06-11 21:43:38 +0300

    signal demo

--- src/app/Default/default.component.ts
+++ src/app/Default/default.component.ts
@@ -9,10 +9,12 @@ export class DefaultComponent {
   price: number = 100;
   total: number = 0;
   quantity: number = 0;
+  updatedTotal: number[] = [];
 
   increase(): void {
     this.quantity++;
     this.total = this.price * this.quantity;
+    this.updatedTotal.push(this.total);
   }
   
   decrease(): void {
@@ -23,5 +25,16 @@ export class DefaultComponent {
       this.quantity = 0;
       this.total = 0;
     }
+    this.updatedTotal.push(this.total);
+  }
+
+  rest(): void {
+    this.updatedTotal.push(0);
+    this.quantity = 0;
+    this.total = 0;
+  }
+
+  restOldTotals() : void {
+    this.updatedTotal = [0];
   }
 }
--- src/app/Signal/signal.component.ts
+++ src/app/Signal/signal.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signal',
@@ -9,8 +9,13 @@ export class SignalComponent {
   price: number = 100;
   total = computed(() => this.price * this.quantity());
   quantity = signal<number>(0);
+  updatedTotal: number[] = [];
 
-  constructor() {}
+  constructor() {
+    effect(()=> {
+      this.updatedTotal.push(this.total());
+    })
+  }
 
   increase(): void {
     // this.quantity.set(this.quantity() + 1);
@@ -25,5 +30,13 @@ export class SignalComponent {
       // this.quantity.set(0);
       this.quantity.update((oldValue) => 0);
     }
+  }
+
+  rest(): void {
+    this.quantity.set(0);
+  }
+
+  restOldTotals() : void {
+    this.updatedTotal = [0];
   }
 }
